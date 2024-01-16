0869ca91d2477021a6ad2263eb3add7bc5adfc09
Maria <mchaparrocaballeroa706@linkiafp.online>
2024-01-16 20:32:14 +0100

    finalizado, comentarios y bonito

--- Ejercicios_POO_II_3/src/EJERCICIO3/Estudiante.java
+++ Ejercicios_POO_II_3/src/EJERCICIO3/Estudiante.java
@@ -1,53 +1,51 @@
 package EJERCICIO3;
 
-public class Estudiante {
+public class Estudiante {//atributos//
 
 	private String Nombre;
 	private int Edad;
 	private double calificacion;
 
-	Estudiante() {
+	Estudiante() {//constructor//
 		Nombre = "Alicia";
 		Edad = 24;
 		calificacion = 8.9;
 	}
 
-	public void set_Nombre(String nNombre) {
-		this.Nombre = nNombre;
+	public void set_Nombre(String Nombre) {//setter//
+		this.Nombre = Nombre;
 
 	}
 
-	public String get_nombre() {
+	public String get_nombre() {//getter//
 		return " es " + Nombre;
 	}
 
-	public void set_Edad(int nEdad) {
-		if(nEdad>=0) {
-			this.Edad = nEdad;	
-		}else {
+	public void set_Edad(int Edad) {//setter//
+		if (Edad >= 0) {//restringimos su funcionamiento en caso de edad negativa//
+			this.Edad = Edad;
+		} else {
 			System.out.println("Error no existen edades negativas");
-			this.Edad=(Integer) null;
+			this.Edad = (Integer) null;
 		}
-		
 
 	}
 
-	public String get_Edad() {
+	public String get_Edad() {//getter//
 		return " es " + Edad;
 	}
 
-	public void set_calificacion(double ncalificacion) {
-		if(ncalificacion>=0 && ncalificacion <=100) {
-			this.calificacion = ncalificacion;	
-		}else {
+	public void set_calificacion(double calificacion) {//setter//
+		if (calificacion >= 0 && calificacion <= 100) {//restringimos su funcionamiento en caso de estar fuera del intervalo deseado//
+			this.calificacion = calificacion;
+		} else {
 			System.out.println("Error esta fuera del rango de calificaciones");
-			this.calificacion= (Double) null;
+			this.calificacion = (Double) null;
 		}
-		
 
 	}
 
-	public String get_calificacion() {
+	public String get_calificacion() {//getter//
 		return " es " + calificacion;
 	}
 }
