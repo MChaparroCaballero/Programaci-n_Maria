daa148feabcc4469c87f8b2050958edcc6d7c36a
Maria <mchaparrocaballeroa706@linkiafp.online>
2024-01-17 13:03:44 +0100

    primera parte

---
+++ ejercicios_POO_III_1/src/ejercicios_POO_III/Uso_RelojAlarma.java
@@ -0,0 +1,10 @@
+package ejercicios_POO_III;
+
+public class Uso_RelojAlarma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ejericios_POO_III_1/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module ejericios_POO_III_1 {
+}
