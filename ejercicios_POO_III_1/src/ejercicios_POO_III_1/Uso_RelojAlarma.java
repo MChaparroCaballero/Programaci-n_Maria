1f791dcf23c14cd061f14971e27d9117bbf43a09
Maria <mchaparrocaballeroa706@linkiafp.online>
2024-01-17 21:51:34 +0100

    idea de como hacer para que introduczca los tiempos de alarma, el while se podra modificar en un futuro con un for para poder si quieres añadir más de una alarma

--- ejercicios_POO_III_1/src/ejercicios_POO_III_1/Uso_RelojAlarma.java
+++ ejercicios_POO_III_1/src/ejercicios_POO_III_1/Uso_RelojAlarma.java
@@ -5,25 +5,27 @@ import java.util.Scanner;
 public class Uso_RelojAlarma {
 
 	public static void main(String[] args) {
-		// 
+		//
+		int i = 1;
 		Scanner lector;
 		lector = new Scanner(System.in);
 		int nhoras = lector.nextInt();
 		int nminutos = lector.nextInt();
-		
-		
-		//nueva instancia de la clase//
-		RelojAlarma nueva = new RelojAlarma();
-		
-		//para introducir nuevos tiempos en los parametros//
-		nueva.set_horaAlarma(nhoras);
-		nueva.set_minutosAlarma(nminutos);
+
+		// para introducir nuevos tiempos en los parametros//
+		while (i > 0) {
+			// nueva instancia de la clase//
+			RelojAlarma nueva = new RelojAlarma();
+			nueva.set_horaAlarma(nhoras);
+			nueva.set_minutosAlarma(nminutos);
+			i = 0;
+		}
 
 	}
-	
-	/*public static int declaramostiempodeAlarma(int horas, int minutos) {
-		int declaramostiempodeAlarma = horas
-	}*/
 
+	/*
+	 * public static int declaramostiempodeAlarma(int horas, int minutos) { int
+	 * declaramostiempodeAlarma = horas }
+	 */
 
 }
