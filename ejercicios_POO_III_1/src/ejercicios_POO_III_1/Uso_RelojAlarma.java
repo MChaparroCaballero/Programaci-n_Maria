dfaf8f3142060d9a8ced1bb7a88afa946c4b11da
Maria <mchaparrocaballeroa706@linkiafp.online>
2024-01-17 21:37:07 +0100

    hacemos scanner y modificamos los setters de las alarmas para poder establecer por comando la que queremos

--- ejercicios_POO_III_1/src/ejercicios_POO_III_1/Uso_RelojAlarma.java
+++ ejercicios_POO_III_1/src/ejercicios_POO_III_1/Uso_RelojAlarma.java
@@ -1,10 +1,29 @@
 package ejercicios_POO_III_1;
 
+import java.util.Scanner;
+
 public class Uso_RelojAlarma {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 
+		Scanner lector;
+		lector = new Scanner(System.in);
+		int nhoras = lector.nextInt();
+		int nminutos = lector.nextInt();
+		
+		
+		//nueva instancia de la clase//
+		RelojAlarma nueva = new RelojAlarma();
+		
+		//para introducir nuevos tiempos en los parametros//
+		nueva.set_horaAlarma(nhoras);
+		nueva.set_minutosAlarma(nminutos);
 
 	}
+	
+	/*public static int declaramostiempodeAlarma(int horas, int minutos) {
+		int declaramostiempodeAlarma = horas
+	}*/
+
 
 }
