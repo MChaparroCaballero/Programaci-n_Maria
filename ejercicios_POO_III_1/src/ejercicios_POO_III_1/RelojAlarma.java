ae8d24c3882975e9ba3ed78ea922b1ea155f6dfc
Maria <mchaparrocaballeroa706@linkiafp.online>
2024-01-17 13:17:41 +0100

    primera parte de la clase getts y setts

---
+++ ejercicios_POO_III_1/src/ejercicios_POO_III_1/RelojAlarma.java
@@ -0,0 +1,86 @@
+package ejercicios_POO_III_1;
+
+public class RelojAlarma {
+	private int horaAlarma;
+
+	private int minutosAlarma;
+
+	private int horaActual;
+
+	private int minutosActual;
+
+	private boolean activada;
+
+	RelojAlarma() {
+
+		horaAlarma = 06;
+
+		minutosAlarma = 25;
+
+		horaActual = 10;
+
+		minutosActual = 24;
+
+	}
+
+	public void set_minutosAlarma(int minutosAlarma) {// setter//
+
+		this.minutosAlarma = minutosAlarma;
+
+	}
+
+	public int get_minutosAlarma() {// getter//
+
+		return minutosAlarma;
+
+	}
+
+	public void set_horaActual(int horaActual) {// setter//
+
+		this.horaActual = horaActual;
+
+	}
+
+	public int get_horaActual() {// getter//
+
+		return horaActual;
+
+	}
+
+	public void set_minutosActual(int minutosActual) {// setter//
+
+		this.minutosActual = minutosActual;
+
+	}
+
+	public int get_minutosActual() {// getter//
+
+		return minutosActual;
+
+	}
+
+	public void set_activada(boolean activada) {
+
+		this.activada = activada;
+
+	}
+
+	public String get_activada() {
+
+		return " la alarma esta activada" + activada;
+
+	}
+
+	public void set_horaAlarma(int horaAlarma) {// setter//
+
+		this.horaAlarma = horaAlarma;
+
+	}
+
+	public int get_horaAlarma() {// getter//
+
+		return horaAlarma;
+
+	}
+
+}
---
+++ ejercicios_POO_III_1/src/ejercicios_POO_III_1/Uso_RelojAlarma.java
@@ -0,0 +1,10 @@
+package ejercicios_POO_III_1;
+
+public class Uso_RelojAlarma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
