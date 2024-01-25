539834f8214890ce5d09be32880d7c0d0727423b
Maria <mchaparrocaballeroa706@linkiafp.online>
2024-01-25 19:41:03 +0100

    pequeños cambios de impresion, es posible que necesite otra pasado porque es system.out suelto no me gusta, buscar como hacer tablas

--- ejercicios_POO_IV_1/src/ejercicios_POO_IV_4/Uso_deProducto.java
+++ ejercicios_POO_IV_1/src/ejercicios_POO_IV_4/Uso_deProducto.java
@@ -9,23 +9,32 @@ import ejercicios_POO_IV_2.Empleado;
 public class Uso_deProducto {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// instanciamos el array de objetos//
 		Producto[] inventario = new Producto[2];
 
 		for (int i = 0; i < inventario.length; i++) {
-
+			/*
+			 * usamos el metodo crear producto para introducir los datos de los objetos por
+			 * cada posicion del array
+			 */
 			inventario[i] = crearproducto();
 
 		}
-		
+		/*
+		 * pongo aqui este print para que no me lo repita cada vez que se ejecute el
+		 * bucle
+		 */
+		System.out.println("Resumen de inventario: ");
 		for (Producto inventarios : inventario) {
-
-		resumenInventario(inventarios);
+			// bucle for each que recorrera el array de objetos mostrando sus datos
+			// utilizando el metodo resuemenInventario//
+			resumenInventario(inventarios);
 
 		}
 
 	}
 
+	/* metodo para introducir datos de los productos */
 	private static Producto crearproducto() {
 		String nombre = JOptionPane.showInputDialog("Introduzca el nombre del producto:");
 		int cantidad = Integer.parseInt(JOptionPane.showInputDialog("Introduzca la cantidad"));
@@ -34,13 +43,13 @@ public class Uso_deProducto {
 		return new Producto(nombre, cantidad, precio);
 
 	}
-	
-	
+
+	/* Metodo con el que se imprimen los datos de cada objetos en modo lista */
 	private static void resumenInventario(Producto inventarios) {
-		System.out.println("Resumen de inventario: ");	
 		System.out.println("Nombre: " + inventarios.getNombre() + "\nCantidad: " + inventarios.getCantidad()
-				+ "\nPrecio/unidad: " + inventarios.getPrecio() + "\ntotal: " + (inventarios.getCantidad() * inventarios.getPrecio()) + " euros");
+				+ "\nPrecio/unidad: " + inventarios.getPrecio() + "\ntotal: "
+				+ (inventarios.getCantidad() * inventarios.getPrecio()) + " euros");
 		System.out.println("------------------------------------------------------------------------");
 	}
-	
+
 }
