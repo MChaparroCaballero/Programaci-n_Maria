cd3f84f0227fd1bb3a228d8b216077f9a0f03191
Maria <mchaparrocaballeroa706@linkiafp.online>
2024-01-31 11:37:07 +0100

    hacemos el constructor pero qued ahcer los metodos y y setters y getters, utilizaremos los emtodos con setters para cambiar la static resultado

--- ejercicios_POO_VII/src/ejercicios_POO_VII_2/Calculadora.java
+++ ejercicios_POO_VII/src/ejercicios_POO_VII_2/Calculadora.java
@@ -4,6 +4,15 @@ public class Calculadora {
 
 	private double operador1;
 	private double operador2;
+	private static double resultado;
+	
+	
+	public Calculadora(double operador1 , double operador2) {
+		this.operador1 = operador1;
+		this.operador2 = operador2;
+		resultado =0;
+		
+	}
 	
 	
 	
