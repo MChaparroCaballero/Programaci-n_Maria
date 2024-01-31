0c55b8fe9cf367b14c2c3ce94ef8aa3fdde6aa74
Maria <mchaparrocaballeroa706@linkiafp.online>
2024-01-31 10:41:12 +0100

    principio del ejercicio 2 de la clase7

---
+++ ejercicios_POO_VII/src/ejercicios_POO_VII_2/Calculadora.java
@@ -0,0 +1,10 @@
+package ejercicios_POO_VII_2;
+
+public class Calculadora {
+
+	private double operador1;
+	private double operador2;
+	
+	
+	
+}
---
+++ ejercicios_POO_VII/src/ejercicios_POO_VII_2/Uso_Calculadora.java
@@ -0,0 +1,10 @@
+package ejercicios_POO_VII_2;
+
+public class Uso_Calculadora {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
