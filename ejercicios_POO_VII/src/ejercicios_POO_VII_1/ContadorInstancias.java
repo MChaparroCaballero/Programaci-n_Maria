5726c3c762e7762b527abcfc85ffc52a0c73952a
Maria <mchaparrocaballeroa706@linkiafp.online>
2024-01-31 10:21:06 +0100

    ejercicio 1 de la clase 7 pero busca como hacerlo todo en un mensaje

--- ejercicios_POO_VII/src/ejercicios_POO_VII_1/ContadorInstancias.java
+++ ejercicios_POO_VII/src/ejercicios_POO_VII_1/ContadorInstancias.java
@@ -1,24 +1,45 @@
 package ejercicios_POO_VII_1;
 
 public class ContadorInstancias {
-	private String nombre;
-	private static int contador;
+	private String nombre; 
 
-	public ContadorInstancias(String nombre) {
-		this.nombre = nombre;
-		ContadorInstancias.contador = 0;
-	}
+	private static int contador; 
 
-	public String getNombre() {
-		return nombre;
-	}
+ 
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
+	public ContadorInstancias(String nombre) { 
 
-	public static int getContador() {
-		return contador = ContadorInstancias.contador +1;
-	}
+		this.nombre = nombre; 
 
-}
+		ContadorInstancias.contador = 0; 
+
+	} 
+
+ 
+
+	public String getNombre() { 
+
+		return nombre; 
+
+	} 
+
+ 
+
+	public void setNombre(String nombre) { 
+
+		this.nombre = nombre; 
+
+	} 
+
+ 
+
+	public static int getContador() { 
+
+		return contador = ContadorInstancias.contador +1; 
+
+	} 
+
+ 
+
+} 
+
--- ejercicios_POO_VII/src/ejercicios_POO_VII_1/Uso_ContadorInstancias.java
+++ ejercicios_POO_VII/src/ejercicios_POO_VII_1/Uso_ContadorInstancias.java
@@ -4,21 +4,29 @@ import java.util.*;
 
 import javax.swing.JOptionPane;
 
+
 public class Uso_ContadorInstancias {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		ContadorInstancias[] nombres = new ContadorInstancias[3];
 
 		for (int i = 0; i < nombres.length; i++) {
+
 			nombres[i] = new ContadorInstancias(JOptionPane.showInputDialog("Ingrese el nombre"));
+
 		}
 
 		// Mostrar información utilizando getters y un bucle//
+
 		for (ContadorInstancias numero : nombres) {
+
 			JOptionPane.showMessageDialog(null,
+
 					"\nNombre: " + numero.getNombre() + "\nInstancias: " + ContadorInstancias.getContador());
+
 		}
 
 	}
+
 }
