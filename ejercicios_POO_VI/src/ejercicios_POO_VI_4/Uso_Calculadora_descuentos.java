1bd44a7a3e1f404a99554bf5485a7b57baca8a97
Maria <mchaparrocaballeroa706@linkiafp.online>
2024-02-01 00:26:36 +0100

    arreglos menu poo clas 6 ejer 4

--- ejercicios_POO_VI/src/ejercicios_POO_VI_4/Uso_Calculadora_descuentos.java
+++ ejercicios_POO_VI/src/ejercicios_POO_VI_4/Uso_Calculadora_descuentos.java
@@ -57,6 +57,10 @@ public class Uso_Calculadora_descuentos {
 				JOptionPane.showMessageDialog(null, "Gracias por usar nuestra app");
 				i = 2;
 				break;
+				
+			default:
+				JOptionPane.showMessageDialog(null, "Error, opción invalida");
+
 				//.//		
 				}
 		}
