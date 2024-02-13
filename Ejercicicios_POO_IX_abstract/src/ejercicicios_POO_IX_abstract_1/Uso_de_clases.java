1a88e2d918f28df645cb9ec4c202ee5b909407e7
Maria <mchaparrocaballeroa706@linkiafp.online>
2024-02-13 19:36:29 +0100

    nuevos ejemplos del eje 1 de la POO IX

--- Ejercicicios_POO_IX_abstract/src/ejercicicios_POO_IX_abstract_1/Uso_de_clases.java
+++ Ejercicicios_POO_IX_abstract/src/ejercicicios_POO_IX_abstract_1/Uso_de_clases.java
@@ -3,8 +3,8 @@ package ejercicicios_POO_IX_abstract_1;
 public class Uso_de_clases {
 
 	public static void main(String[] args) {
-		Rectangulo R = new Rectangulo(11, 6);
-		Circulo C = new Circulo(3);
+		Rectangulo R = new Rectangulo(5, 10);
+		Circulo C = new Circulo(5.5);
  
 		System.out.println("El área del rectángulo es: " + R.calcularArea());
 		System.out.println("El área del círculo es: " + C.calcularArea());
