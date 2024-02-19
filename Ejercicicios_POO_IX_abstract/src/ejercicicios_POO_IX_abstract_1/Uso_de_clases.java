cbb949c3ca3db6311def693685068c4d91659149
Maria <mchaparrocaballeroa706@linkiafp.online>
2024-02-19 10:42:04 +0100

    corregimos el ejercicio 1 de la clase poo IX ABSTRACT ESTA PERFECTO

--- Ejercicicios_POO_IX_abstract/src/ejercicicios_POO_IX_abstract_1/Uso_de_clases.java
+++ Ejercicicios_POO_IX_abstract/src/ejercicicios_POO_IX_abstract_1/Uso_de_clases.java
@@ -1,13 +1,15 @@
 package ejercicicios_POO_IX_abstract_1;
 
+import javax.swing.JOptionPane;
+
 public class Uso_de_clases {
 
 	public static void main(String[] args) {
 		Rectangulo R = new Rectangulo(5, 10);
 		Circulo C = new Circulo(5.5);
  
-		System.out.println("El área del rectángulo es: " + R.calcularArea());
-		System.out.println("El área del círculo es: " + C.calcularArea());
+		JOptionPane.showMessageDialog(null, "El área del rectángulo es: " + R.calcularArea());
+		JOptionPane.showMessageDialog(null, "El área del círculo es: " + C.calcularArea());
 	
 
 	}
