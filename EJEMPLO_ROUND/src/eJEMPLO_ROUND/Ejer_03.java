7ec685a7f3a9b1bc54468eaa3ee9dd04752b34d4
Maria <mchaparrocaballeroa706@linkiafp.online>
2024-02-21 14:02:27 +0100

    ejer 3

---
+++ EJEMPLO_ROUND/src/eJEMPLO_ROUND/Ejer_03.java
@@ -0,0 +1,15 @@
+package eJEMPLO_ROUND;
+
+import java.util.ArrayList;
+
+import Clases.Persona;
+public class Ejer_03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Persona> lista = new Arraylist<Persona>();
+		
+		lista.add(new Persona("Maria",20));
+	}
+
+}
