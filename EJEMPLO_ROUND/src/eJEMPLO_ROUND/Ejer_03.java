aa23fcf8f13a1b59646b36280134d5b1951f7603
Maria <mchaparrocaballeroa706@linkiafp.online>
2024-02-26 12:34:20 +0100

    clase rayo hashcode

--- EJEMPLO_ROUND/src/eJEMPLO_ROUND/Ejer_03.java
+++ EJEMPLO_ROUND/src/eJEMPLO_ROUND/Ejer_03.java
@@ -7,9 +7,23 @@ public class Ejer_03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ArrayList<Persona> lista = new Arraylist<Persona>();
+		ArrayList<Persona> lista = new ArrayList<Persona>();
 		
 		lista.add(new Persona("Maria",20));
+		lista.add(new Persona("Ana",20));
+		
+		Persona p1 = new Persona ("Ana",20);
+		Persona p2 = new Persona ("Ana",20);
+		
+		
+		System.out.println(p1.hashCode());
+		System.out.println(p2.hashCode());
+		
+		if(p1.equals(p2)) {
+			System.out.println("somos iguales");
+		}else {
+			System.out.println("somos distintos");
+		}
 	}
 
 }
