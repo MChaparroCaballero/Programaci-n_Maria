748fb78643f99d72b57770d4580eeff70e40e0ee
Maria <mchaparrocaballeroa706@linkiafp.online>
2024-02-28 13:41:52 +0100

    mas hasmap rayo

---
+++ EJEMPLO_ROUND_3_INTERFACES_RAYO/src/main/DiccionarioCastellanoIngles.java
@@ -0,0 +1,24 @@
+package main;
+
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class DiccionarioCastellanoIngles {
+static Scanner sc=new Scanner(System.in);
+	public static void main(String[] args) {
+		HashMap<String,String> lista= new HashMap<String,String>();
+		String castellano,ingles;
+		while(true) {
+			System.out.print("Dime la palabra en castellano");
+			castellano=sc.nextLine();
+			if(castellano.compareToIgnoreCase("FIN")==0) break;
+			if(lista.containsKey(castellano)) {
+				System.out.println("La traduccion es "+lista.get(castellano));
+			}else {
+				System.out.print("Dime la traducción");
+				ingles=sc.nextLine();
+			}
+		}
+	}
+
+}
