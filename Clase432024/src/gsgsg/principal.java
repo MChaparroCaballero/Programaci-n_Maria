76e31f276b5c3aba0dafe19e3af9c78e9eb3bbfa
Maria <mchaparrocaballeroa706@linkiafp.online>
2024-03-04 13:11:41 +0100

    pasamos a octal, hexadecimal, binario, con algoritmos

--- Clase432024/src/gsgsg/principal.java
+++ Clase432024/src/gsgsg/principal.java
@@ -23,10 +23,11 @@ public class principal {
 		}
 	}
 
-	public static String d_a_b(int numero) {//decimal a binario//
-		//string que es para dar espacio//
+	public static String d_a_b(int numero) {// decimal a binario//
+		// string que es para dar espacio//
 		String cadena = "";
-		//bucle en el cual mientras numero sea mayor que 0 la cadena sera un string del valor que tendria el resto de numero/2 + el espacio de cadena
+		// bucle en el cual mientras numero sea mayor que 0 la cadena sera un string del
+		// valor que tendria el resto de numero/2 + el espacio de cadena
 		while (numero > 0) {
 			cadena = String.valueOf(numero % 2) + cadena;
 			numero = (int) (numero / 2);
@@ -35,12 +36,22 @@ public class principal {
 		return cadena;
 	}
 
-	public static String d_a_h(int numero) {//decimal a hexadecimal//
+	public static String d_a_h(int numero) {// decimal a hexadecimal//
 		String cadenita = "0123456789ABCDEF";
 		String cadena = "";
 		while (numero > 0) {
-			cadena = cadenita.charAt(numero % 16) + cadena;//obtenemos la posicion segun la cadena //
+			cadena = cadenita.charAt(numero % 16) + cadena;// obtenemos la posicion segun la cadena //
 			numero = (int) (numero / 16);
+		}
+		return cadena;
+	}
+
+	public static String d_a_o(int numero) {// decimal a hexadecimal//
+		int vector[] = { 0, 1, 2, 3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 15, 16, 17 };
+		String cadena = "";
+		while (numero > 0) {
+			cadena = vector[numero % 8] + cadena;// obtenemos la posicion segun la cadena //
+			numero = (int) (numero / 8);
 		}
 		return cadena;
 	}
@@ -59,8 +70,18 @@ public class principal {
 		}
 		String cadenita = "0123456789ABCDEF";
 		for (int elemento : tb) {
-			System.out.printf("valor=%5d binario=%8s\n", elemento, d_a_h(elemento));
+			System.out.printf("valor=%5d hexadecimal=%8s\n", elemento, d_a_h(elemento));
 		}
+
+		for (int elemento : tb) {
+			System.out.printf("valor=%5d octal=%8s\n", elemento, d_a_o(elemento));
+		}
+
+		for (int elemento : tb) {
+			System.out.printf("valor=%5d binario=%9s hexadecimal=%8s octal=%8s\n", elemento, d_a_b(elemento),
+					d_a_h(elemento), d_a_o(elemento));
+		}
+
 	}
 
 }
