e10e91cc41e04b2ea86d08108f91a6f4806d9483
Maria <mchaparrocaballeroa706@linkiafp.online>
2024-04-12 08:45:28 +0200

    explicaion de indexof y substring

--- EJERFICHEROS2/src/class10_4_2024/Ejer03.java
+++ EJERFICHEROS2/src/class10_4_2024/Ejer03.java
@@ -5,31 +5,36 @@ import java.io.FileReader;
 import java.util.Scanner;
 
 public class Ejer03 {
-	
-	public static Scanner sc = new Scanner(System.in);
 
+	public static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		// crea un programa que cree un fichero y busque una palabra que pides por
 		// pantalla en le fichero y que nos muestre cuantas veces se la encuentra en el
 		// fichero en la misma linea */
 
-		/*ruta esta incompleta*/
-		String cadena="Hola jose como estais jose maria andres jose manuel",palabra="jose";
-		int n=cadena.indexOf("jose"),c=0;
-		
-		while(n>-1) {
+		/* ruta esta incompleta */
+		String cadena = "Hola jose como estais jose maria andres jose manuel", palabra = "jose";
+		int n = cadena.indexOf("jose"), c = 0;
+
+		while (n > -1) {
 			c++;
-			cadena=cadena.substring(n+palabra.length());
-			 n=cadena.indexOf(palabra);
+			/*
+			 * ahi decimes que ahora cadena empieza desde la siguiente posicion donde
+			 * encontro jose por eso desde donde empieza la palabra + la longitud de la
+			 * palabra 
+			 */
+			cadena = cadena.substring(n + palabra.length());
+			/*ahora n sera igual a el nuevo valor de cadena y como es un bucle pues ta ta ta lo va haciendo hasta que se termine*/
+			n = cadena.indexOf(palabra);
 		}
-		
+
 		System.out.println(c);
 		/*
-		System.out.println(cadena.substring(5));
-		System.out.println(cadena.substring(2, 6));*/
-		
-		
+		 * System.out.println(cadena.substring(5));
+		 * System.out.println(cadena.substring(2, 6));
+		 */
+
 	}
 
 }
