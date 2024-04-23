5b1c58f4900977ea08d0f482f71d30416d91a0f8
Maria <mchaparrocaballeroa706@linkiafp.online>
2024-04-23 13:55:24 +0200

    hacemos hasta el 3 revisar

---
+++ deberes/src/ejer03/Movil.java
@@ -0,0 +1,74 @@
+package ejer03;
+
+import java.text.DecimalFormat;
+
+public class Movil extends Terminal {
+
+	private String tarifa;
+	private double coste;
+	private int tiempo_conversacion;
+
+	public Movil(String numero, String tarifa) {
+		super(numero);
+		this.tarifa = tarifa;
+		this.coste = 0;
+		this.tiempo_conversacion=0;
+	}
+
+	public String getTarifa() {
+		return tarifa;
+	}
+
+	public void setTarifa(String tarifa) {
+		this.tarifa = tarifa;
+	}
+
+	public double getCoste() {
+		return coste;
+	}
+
+	public void setCoste(double coste) {
+		this.coste = coste;
+	}
+	
+	
+	
+
+	public int getTiempo_conversacion() {
+		return tiempo_conversacion;
+	}
+
+	public void setTiempo_conversacion(int tiempo_conversacion) {
+		this.tiempo_conversacion = tiempo_conversacion;
+	}
+
+	
+	public void llama(Movil a, int t) {
+		// TODO Auto-generated method stub
+
+		this.setTiempo_conversacion(this.getTiempo_conversacion() + t);
+		a.setTiempo_conversacion(a.getTiempo_conversacion() + t);
+	
+		
+		if (this.getTarifa().equalsIgnoreCase("rata")) {
+			this.setCoste(this.getCoste() + (((t / 60) * 6) / 100));
+
+		} else if (this.getTarifa().equalsIgnoreCase("mono")) {
+			this.setCoste(this.getCoste() + (((t / 60) * 12) / 100));
+		} else if (this.getTarifa().equalsIgnoreCase("bisonte")) {
+			this.setCoste(this.getCoste() + (((t / 60) * 30) / 100));
+		} else {
+			System.out.print("error tarifa inexistente");
+		}
+
+	}
+
+	@Override
+	public String toString() {
+		DecimalFormat df = new DecimalFormat("#0.00");
+		return "Nº " + this.getNumero() + " - " + this.getTiempo_conversacion() + " s de conversación"+ " - tarifados " + df.format(this.getCoste());
+	}
+
+	
+
+}
