c1e65fc069f43ef836aa7ba10b3492e2f67e7eee
Maria <mchaparrocaballeroa706@linkiafp.online>
2024-05-17 13:32:52 +0200

    F

--- Ejercicio03AlmacenBBDD/src/principal/Principal.java
+++ Ejercicio03AlmacenBBDD/src/principal/Principal.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 import CLASES.Articulo;
+import CLASES.Generales;
 
 public class Principal {
 
@@ -34,12 +35,8 @@ public class Principal {
 	}
 
 	public static void CrearTB() {
-		String cadena = "CREATE TABLE ARTICULOS(" 
-				+ "ID INT NOT NULL AUTO_INCREMENT," 
-				+ "DESCRIPCION VARCHAR(500),"
-				+ "PRECIO_DE_COMPRA DECIMAL(10,2)," 
-				+ "PRECIO_DE_VENTA DECIMAL(10,2)," 
-				+ "STOCK INT CHECK(STOCK>=0),"
+		String cadena = "CREATE TABLE ARTICULOS(" + "ID INT NOT NULL AUTO_INCREMENT," + "DESCRIPCION VARCHAR(500),"
+				+ "PRECIO_DE_COMPRA DECIMAL(10,2)," + "PRECIO_DE_VENTA DECIMAL(10,2)," + "STOCK INT CHECK(STOCK>=0),"
 				+ "PRIMARY KEY(ID)" + ")";
 		try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/GESTISIMAL", "root", "ROOT");
 				PreparedStatement sentencia = con.prepareStatement(cadena);) {
@@ -54,23 +51,6 @@ public class Principal {
 		}
 	}
 
-	public static void AÑADIRDATOS() {
-		String cadena = "INSERT INTO ARTICULOS(DESCRIPCION,PRECIO_DE_COMPRA,PRECIO_DE_VENTA,STOCK) VALUES(?,?,?,?)";
-		try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/GESTISIMAL", "root", "ROOT");
-				PreparedStatement sentencia = con.prepareStatement(cadena);) {
-			sentencia.setString(1, "jose manuel");
-			sentencia.setTimestamp(2, Timestamp.valueOf(LocalDate.of(1990, 1, 5).atTime(0, 0)));
-			int row = sentencia.executeUpdate();
-
-			System.out.println("Insertados " + row + " registros.");
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-
-	}
-
 	public static void InsertarTablaArrayList() {
 		int row = 0;
 		ArrayList<Articulo> lista = new ArrayList<Articulo>();
@@ -78,19 +58,20 @@ public class Principal {
 		lista.add(new Articulo(2, "especias", 15, 20, 50));
 		lista.add(new Articulo(3, "limpieza", 20, 25, 100));
 		String cadena = "INSERT INTO ARTICULOS(DESCRIPCION,PRECIO_DE_COMPRA,PRECIO_DE_VENTA,STOCK) VALUES(?,?,?,?)";
-		//las interrogantes simbolizan cada una un campo//
+		// las interrogantes simbolizan cada una un campo//
 		try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/GESTISIMAL", "root", "ROOT");
 				PreparedStatement sentencia = con.prepareStatement(cadena);) {
 
 			for (Articulo a : lista) {
 
-				//para cada resgristro de aqui tenemos que cojer los datos relativa del l y remplazarlos en la sentencia//
-				sentencia.setString(1, a.getDESCRIPCION());//asigna a la primera interrogante//
-				sentencia.setDouble(2, a.getPRECIO_DE_COMPRA());//asigna a la segunda interrogante//
-				sentencia.setDouble(3, a.getPRECIO_DE_VENTA());//asigna a la tercera interrogante//
-				sentencia.setInt(4, a.getSTOCK());//asigna a la cuarta interrogante//
+				// para cada resgristro de aqui tenemos que cojer los datos relativa del l y
+				// remplazarlos en la sentencia//
+				sentencia.setString(1, a.getDESCRIPCION());// asigna a la primera interrogante//
+				sentencia.setDouble(2, a.getPRECIO_DE_COMPRA());// asigna a la segunda interrogante//
+				sentencia.setDouble(3, a.getPRECIO_DE_VENTA());// asigna a la tercera interrogante//
+				sentencia.setInt(4, a.getSTOCK());// asigna a la cuarta interrogante//
 
-				row += sentencia.executeUpdate();//nos da el numero de filas actualizadas,//
+				row += sentencia.executeUpdate();// nos da el numero de filas actualizadas,//
 			}
 			System.out.println("Insertados " + row + " registros.");
 
@@ -102,22 +83,28 @@ public class Principal {
 
 	}
 
-	public static void consulta() {
+	
+
+	public static void listado() {
 
 		String cadena = "SELECT * FROM ARTICULOS";
-		cadena = cadena.replace(",", ".");// Para cambiar comas por puntos
 
 		try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/GESTISIMAL", "root", "ROOT");
 				PreparedStatement sentencia = con.prepareStatement(cadena);) {
-			ResultSet resultado = sentencia.executeQuery();
-			while (resultado.next()) {
-				System.out.printf("ID=%5d\n DESCRIPCION=%5s\n PRECIO_DE_COMPRA=%5s\n PRECIO_DE_VENTA=%5s\n STOCK=%5d\n",
-						resultado.getInt("ID"), resultado.getString("DESCRIPCION"),
-						resultado.getDouble("PRECIO_DE_COMPRA"), resultado.getDouble("PRECIO_DE_VENTA"),
-						resultado.getInt("STOCK"));
+
+			ResultSet r1 = sentencia.executeQuery();// EL REGISTRO//
+
+			// MIENTRAS HALLA SIGUIENTE NOS IMRPIME EL REGISTRO//
+			while (r1.next()) {
+				System.out.printf("ID=%4d DESCRIPCION=%50s PRECIO_DE_COMPRA=%10.2f PRECIO_DE_VENTA=%10.2f STOCK=%5d\n",
+						r1.getInt("ID"), r1.getString("DESCRIPCION"), r1.getDouble("PRECIO_DE_COMPRA"),
+						r1.getDouble("PRECIO_DE_VENTA"), r1.getInt("STOCK"));
+				// eso es para obtener los datos alamcenados en las clases que representan cada
+				// uno un registro//
 
 			}
-
+			// para los formatos el print f enteros es d, string s, los decimales es .2f y
+			// la \n final es que le indicamos que nos salte de linea)//
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		} catch (Exception e) {
@@ -126,60 +113,70 @@ public class Principal {
 
 	}
 
-	
-	
-	public static void ActualizaTabla(double incremento) {
+	public static void alta() {
 
-		String cadena = "UPDATE MONEDERO SET CANTIDAD=CANTIDAD + ?";
-		cadena = cadena.replace(",", ".");// Para cambiar comas por puntos
+		// las variables de campos que cabiamos, como tenemos autoincementane la de id
+		// es opcional lo unico que cambiaria es que quitas una interrogante//
+		int stock;
+		String descripcion;
+		double Preciocompra, Precioventa;
+		/********************************************/
+		// System.out.print("Dime el codigo / id");ID=Integer.valueOf(sc.nextLine());//
+		System.out.print("Dime la descripcion");
+		descripcion = sc.nextLine();
+		System.out.print("Dime el precio compra");
+		Preciocompra = Double.valueOf(sc.nextLine());
+		System.out.print("Dime el precio venta");
+		Precioventa = Double.valueOf(sc.nextLine());
+		System.out.print("Dime el stock");
+		stock = Integer.valueOf(sc.nextLine());
 
-		try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/CLASELINKIAFP", "root", "ROOT");
+		/********************************************/
+		String cadena = "INSERT INTO ARTICULOS(DESCRIPCION,PRECIO_DE_COMPRA,PRECIO_DE_VENTA,STOCK) VALUES(?,?,?,?)";
+		//siempre que no actualizamos todos los interrogantes poenmos los interrogante//
+		//si es actualizar lo ponemos asi//
+		int row = 0;
+		// las interrogantes simbolizan cada una un campo//
+		try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/GESTISIMAL", "root", "ROOT");
 				PreparedStatement sentencia = con.prepareStatement(cadena);) {
-			sentencia.setDouble(1, incremento);
-			int row = sentencia.executeUpdate();
-			System.out.println("Numero de filas actualizadas " + row);
 
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
+			// para cada resgristro de aqui tenemos que cojer los datos relativa del l y
+			// remplazarlos en la sentencia//
+			sentencia.setString(1, descripcion);// asigna a la primera interrogante//
+			sentencia.setDouble(2, Preciocompra);// asigna a la segunda interrogante//
+			sentencia.setDouble(3, Precioventa);// asigna a la tercera interrogante//
+			sentencia.setInt(4, stock);// asigna a la cuarta interrogante//
+
+			row += sentencia.executeUpdate();// nos da el numero de filas actualizadas,//
+			System.out.println("El numero de registros insertados es " + row);
+
+		} catch (SQLException f) {
+			System.out.println(f.getMessage());
+		} catch (Exception z) {
+			System.out.println(z.getMessage());
 		}
-
 	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// CrearBBDD();//
-		//CrearTB();//
+		// CrearTB();//
 		// InsertarTablaArrayList();//
 
 		int op;
 		String[] opciones = { "1.listar", "2.mostrar alta", "3.baja", "4.modificación", "5.Entrada de mercancía",
 				"6.salida de mercancia", "7.salir" };
 		do {
-			op = CLASES.Generales.menu(opciones, sc);
+			op = Generales.menu(opciones, sc);
 			switch (op) {
 			case 1:
-				consulta();
+				listado();
 				break;
 
 			case 2:
+				alta();
 				break;
 
-			case 3:
-
-				break;
-
-			case 4:
-				break;
-
-			case 5:
-
-				break;
-			case 6:
-				break;
-
-			case 7:
-				break;
 			}
 		} while (op != opciones.length);
 
